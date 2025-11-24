093e5d4e1313d768e38ff423fc63449b92c6c256
Ionut Tanvuia <itanvuia@c1r7s1.42london.com>
2025-11-24 11:45:11 +0000

    added headers

--- main.c
+++ main.c
@@ -3,15 +3,6 @@
 
 int main(void)
 {
-    int a = 0;
-    int *aa = &a;
-    /*long m = -2147483648;
-    long mx = 2147483647;
-    long *min = &m;
-    long *max = &mx;*/
-
-
-    ft_printf(" %p ", aa);
-    write(1, "\n", 1);
-    printf(" %p %p ", aa, aa);
+	char aa = 'A';
+	ft_printf(" %c ", aa);
 }
